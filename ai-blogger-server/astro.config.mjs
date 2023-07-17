f0f76dea45cd2d8de0389b305c8f4ca15f56701c
marezelej <marlej1997@gmail.com>
2023-07-17 15:34:47 +0200

    Use hybrid site build

--- ai-blogger-server/astro.config.mjs
+++ ai-blogger-server/astro.config.mjs
@@ -3,7 +3,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
+  output: 'hybrid',
   adapter: vercel({
     analytics: true,
   }),
